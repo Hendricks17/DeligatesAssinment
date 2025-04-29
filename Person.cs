e866b5b6387c15e911034b347ca4080885f0b204
Jonathan <Jonathan@DESKTOP-K9GUPS5>
2025-04-29 11:46:21 +0100

    Add project files.

---
+++ Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public class Person
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Gender { get; set; }
+    public string Address { get; set; }
+    public string PhoneNumber { get; set; }
+    public int HeightCm { get; set; }
+
+    public Person(string firstName, string lastName, string gender, string address, string phoneNumber, int heightCm)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Gender = gender;
+        Address = address;
+        PhoneNumber = phoneNumber;
+        HeightCm = heightCm;
+    }
+
+    public string Introduce()
+    {
+        return $"Hi, My name is {FirstName} {LastName}, {Gender}, I reside at {Address}";
+    }
+
+    public int ConvertHeight(string unit)
+    {
+        if (unit.ToLower() == "meter")
+            return HeightCm / 100;
+        else if (unit.ToLower() == "feet")
+            return (int)(HeightCm / 30.48);
+        else
+            throw new ArgumentException("Unit must be 'meter' or 'feet'");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main()
+    {
+        Person person = new Person("Frank", "Castle", "Male", "123 Street", "1234567890", 180);
+
+        // 1. Action deligate taking only parameter
+        Action<string> introduceAction = (msg) => Console.WriteLine(person.Introduce());
+
+        introduceAction("intro");
+        Console.WriteLine();
+        Console.WriteLine();
+
+        // 2. Func delegate taking one heiht parameter(string) and returning int
+        Func<string, int> convertHeight = person.ConvertHeight;
+        Console.WriteLine("Height in meters: " + convertHeight("meter"));
+        Console.WriteLine("Height in feet: " + convertHeight("feet"));
+
+        Console.WriteLine();
+        Console.WriteLine();
+
+        // 3. Predicate delegate returning a bool
+        Predicate<string> startsWithA = s => s.StartsWith("A");
+        Console.WriteLine("Starts with A? " + startsWithA("Alice"));
+
+        Console.WriteLine();
+        Console.WriteLine();
+
+        // 4. Lambdas
+        Action<string> introduceLambda = (msg) => Console.WriteLine($"Hi, I'm {person.FirstName}, {person.Gender}, living at {person.Address}");
+        introduceLambda("intro");
+
+        Func<string, int> heightLambda = unit => unit == "meter" ? person.HeightCm / 100 : (int)(person.HeightCm / 30.48);
+        Console.WriteLine("Height with lambda (feet): " + heightLambda("feet"));
+
+        Predicate<string> startsWithALambda = s => s.StartsWith("A");
+        Console.WriteLine("Lambda Predicate: " + startsWithALambda("Andrew"));
+
+        Console.WriteLine();
+        Console.WriteLine();
+
+        // List of Persons
+        List<Person> people = new List<Person>
+        {
+            person,
+            new Person("Jonathan", "Bajela", "Male", "456 Avenue", "0987654321", 165),
+            new Person("Yemi", "Bajela", "Female", "789 Road", "5678901234", 175)
+        };
+
+        // Task.WhenAll
+        var tasks = people.Select(p =>
+            Task.Run(() =>
+            {
+                int heightFeet = p.ConvertHeight("feet");
+                Console.WriteLine($"{p.FirstName}'s height in feet: {heightFeet}");
+            })
+        );
+
+        await Task.WhenAll(tasks);
+    }
+}
